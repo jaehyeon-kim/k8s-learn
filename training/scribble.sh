bd41dae30c2a7e1facf2ff2a5b51a22e280fd4af
Jaehyeon Kim <jakim@networkten.com.au>
2019-09-27 15:44:19 +1000

    services done

--- training/scribble.sh
+++ training/scribble.sh
@@ -13,7 +13,7 @@ http://<minikube-ip>:32000/#scale=2.0
 
 http://192.168.99.100:32000/#scale=2.0
 
-watch -n 1 kubectl get pods,deploy,rs,svc --show-labels
+watch -n 1 kubectl get pods,deploy,rs,svc,ns --show-labels
 
 minikube dashboard
 
@@ -31,10 +31,17 @@ kubectl get pod -o yaml
 
 # if local
 kubectl port-forward vote 8000:80
-
+# 127.0.0.1:8000
 #
 kubectl apply -f training/bootcamp/src/pods/db-pod.yml
-# check if directory exists in host and mounted in pod
+## check if directory exists in host and mounted in pod
+# host - minikube
+minikube ssh
+sudo su -
+ls -alt /var/lib/postgres
+# container
+kubectl exec -it db sh
+ls -alt /var/lib/postgresql/data
 
 kubectl apply -f training/bootcamp/src/pods/multi_container_pod.yml
 
@@ -43,6 +50,8 @@ kubectl exec -it web sh -c sync
 
 kubectl logs web -c nginx
 kubectl logs web -c sync
+
+kubectl delete -f training/bootcamp/src/pods/
 
 #### 07 Replacation Controllers and Replica Sets
 export srcpath=training/bootcamp/src
@@ -53,7 +62,8 @@ kubectl get ns
 
 kubectl apply -f $srcpath/projects/instavote/instavote-ns.yml
 
-kubectl config set-context $(kubectl config current-context) --namespace=instavote
+kubectl config set-context \
+    $(kubectl config current-context) --namespace=instavote
 # Context "minikube" modified.
 
 # deploy to new namespace
@@ -68,3 +78,41 @@ kubectl apply -f $srcpath/projects/instavote/dev/vote-rs.yml
 #   individually editing a pod doesn't update pod <-- should use deployment
 #   if a pod is terminated (kubectl delete pod/<name>), a new pod will be created
 
+kubectl delete -f $srcpath/projects/instavote/dev/
+
+#### 08 Service Discovery and Load Balancing
+
+kubectl apply -f $srcpath/projects/instavote/dev/vote-rs.yml
+kubectl apply -f $srcpath/projects/instavote/dev/vote-svc.yml
+
+# cluster IP is constant but not Pod IPs
+kubectl describe svc vote
+# Name:                     vote
+# Namespace:                instavote
+# Labels:                   role=vote
+# Selector:                 role=vote,version=v1
+# Type:                     NodePort
+# IP:                       10.111.150.233 <- Cluster IP
+# Port:                     <unset>  80/TCP
+# TargetPort:               80/TCP
+# NodePort:                 <unset>  30000/TCP
+# Endpoints:                172.17.0.3:80,172.17.0.4:80 <- POD IPs
+# Session Affinity:         None
+# External Traffic Policy:  Cluster
+# Events:                   <none>
+
+minikube service vote --namespace instavote --url
+# http://192.168.99.100:30000
+
+## Service Discovery
+kubectl exec -it pod/vote-xxx sh
+watch ping redis
+# FROM: ping bad address 'redis'
+
+kubectl apply -f $srcpath/projects/instavote/dev/redis-deploy.yml
+kubectl apply -f $srcpath/projects/instavote/dev/redis-svc.yml
+
+# TO: PING redis (10.107.2.138): 56 data bytes
+# voting should work on http://192.168.99.100:30000/
+
+kubectl delete -f $srcpath/projects/instavote/dev/
