aac3f1d8a22f4496b70c426141b66ca23f32f643
Jaehyeon Kim <jakim@networkten.com.au>
2019-10-02 17:38:31 +1000

    doing day 2

--- training/kube7days/scribble.sh
+++ training/kube7days/scribble.sh
@@ -61,3 +61,57 @@ kubectl create -f kubernetes/
 curl $(minikube ip):30000
 
 watch -n 1 kubectl get pods,deploy,rs,svc,ns --show-labels
+
+######## Day 2
+#### kubernetes manifests
+# https://kubernetes.io/docs/reference/generated/kubernetes-api/v1.16/
+kubectl api-versions
+
+#### kubectl config
+kubectl config view
+
+# cat ~/.kube/config
+
+# apiVersion: v1
+# clusters:
+# - cluster:
+#     certificate-authority: /home/jaehyeon/.minikube/ca.crt
+#     server: https://192.168.99.100:8443
+#   name: minikube
+# contexts:
+# - context:
+#     cluster: minikube
+#     user: minikube
+#   name: minikube
+# current-context: minikube
+# kind: Config
+# preferences: {}
+# users:
+# - name: minikube
+#   user:
+#     client-certificate: /home/jaehyeon/.minikube/client.crt
+#     client-key: /home/jaehyeon/.minikube/client.key
+
+# context --> a cluster and a user are mapped together, optionally set namespace
+kubectl config set-context system --cluster=minikube --user=minikube --namespace=kube-system
+kubectl config use-context system
+
+kubectl config set-context \
+    $(kubectl config current-context) --namespace=<ns-name>
+
+#### helm for package management
+# https://hub.kubeapps.com/
+# https://github.com/helm/charts
+
+# helm init not working in minikube with kubernetes v1.16.0
+# download image manually
+eval $(minikube docker-env)
+docker pull gcr.io/kubernetes-helm/tiller:v2.14.3
+
+# update manifests
+helm init --service-account tiller \
+    --override spec.selector.matchLabels.'name'='tiller',spec.selector.matchLabels.'app'='helm' --output yaml \
+    | sed 's@apiVersion: extensions/v1beta1@apiVersion: apps/v1@' > tiller.yml
+
+kubectl apply -f tiller.yml
+
