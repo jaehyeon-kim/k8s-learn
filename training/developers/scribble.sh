c868f8853f357414471defeb9df1353a2cb46e22
Jaehyeon Kim <jakim@networkten.com.au>
2019-10-04 09:49:14 +1000

    add submodules

--- training/developers/scribble.sh
+++ training/developers/scribble.sh
@@ -1,8 +1,19 @@
+git submodule add https://github.com/kubernetes-for-developers/kfd-flask.git training/developers/modules/kfd-flask
+git submodule add https://github.com/kubernetes-for-developers/kfd-nodejs.git training/developers/modules/kfd-nodejs
+git submodule add https://github.com/kubernetes-for-developers/kfd-celery.git training/developers/modules/kfd-celery
+
+Delete the relevant line from the .gitmodules file.
+Delete the relevant section from .git/config.
+Run git rm --cached path_to_submodule (no trailing slash).
+Commit the superproject.
+Delete the now untracked submodule files.
+
+
 minikube start \
     --cpus 2 \
     --memory 4096 \
     --kubernetes-version v1.15.4 \
-    --insecure-registry 10.0.0.0/24
+    --insecure-registry 10.0.0.0/24s
 
 minikube status
 kubectl cluster-info
