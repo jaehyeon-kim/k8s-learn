bea45e44a3995bff81fb1b48ef1f9a361d87b9d9
Jaehyeon Kim <jakim@networkten.com.au>
2019-10-04 10:08:05 +1000

    update submodules

--- training/developers/submodule-notes.sh
+++ training/developers/submodule-notes.sh
@@ -1,34 +1,35 @@
-git submodule add https://github.com/kubernetes-for-developers/kfd-flask.git training/developers/modules/kfd-flask
-git submodule add https://github.com/kubernetes-for-developers/kfd-nodejs.git training/developers/modules/kfd-nodejs
-git submodule add https://github.com/kubernetes-for-developers/kfd-celery.git training/developers/modules/kfd-celery
+git submodule add https://github.com/jaehyeon-kim/kfd-flask.git apps/kfd-flask
+git submodule add https://github.com/jaehyeon-kim/kfd-nodejs.git apps/kfd-nodejs
+git submodule add https://github.com/jaehyeon-kim/kfd-celery.git apps/kfd-celery
 
-Delete the relevant line from the .gitmodules file.
-Delete the relevant section from .git/config.
-Run git rm --cached path_to_submodule (no trailing slash).
-Commit the superproject.
-Delete the now untracked submodule files.
+### remove submodule
+# Delete the relevant line from the .gitmodules file.
+# Delete the relevant section from .git/config.
+# Run git rm --cached path_to_submodule (no trailing slash).
+# Commit the superproject.
+# Delete the now untracked submodule files.
 
 cat .gitmodules
-# [submodule "training/developers/modules/kfd-flask"]
-#         path = training/developers/modules/kfd-flask
-#         url = https://github.com/kubernetes-for-developers/kfd-flask.git
-# [submodule "training/developers/modules/kfd-nodejs"]
-#         path = training/developers/modules/kfd-nodejs
-#         url = https://github.com/kubernetes-for-developers/kfd-nodejs.git
-# [submodule "training/developers/modules/kfd-celery"]
-#         path = training/developers/modules/kfd-celery
-#         url = https://github.com/kubernetes-for-developers/kfd-celery.git
+# [submodule "apps/kfd-flask"]
+#         path = apps/kfd-flask
+#         url = https://github.com/jaehyeon-kim/kfd-flask.git
+# [submodule "apps/kfd-nodejs"]
+#         path = apps/kfd-nodejs
+#         url = https://github.com/jaehyeon-kim/kfd-nodejs.git
+# [submodule "apps/kfd-celery"]
+#         path = apps/kfd-celery
+#         url = https://github.com/jaehyeon-kim/kfd-celery.git
 
 cat .git/config
 # ...
-# [submodule "training/developers/modules/kfd-flask"]
-#         url = https://github.com/kubernetes-for-developers/kfd-flask.git
+# [submodule "apps/kfd-flask"]
+#         url = https://github.com/jaehyeon-kim/kfd-flask.git
 #         active = true
-# [submodule "training/developers/modules/kfd-nodejs"]
-#         url = https://github.com/kubernetes-for-developers/kfd-nodejs.git
+# [submodule "apps/kfd-nodejs"]
+#         url = https://github.com/jaehyeon-kim/kfd-nodejs.git
 #         active = true
-# [submodule "training/developers/modules/kfd-celery"]
-#         url = https://github.com/kubernetes-for-developers/kfd-celery.git
+# [submodule "apps/kfd-celery"]
+#         url = https://github.com/jaehyeon-kim/kfd-celery.git
 #         active = true
 
 # first time
